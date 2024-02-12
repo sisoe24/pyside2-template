4c6f5ecd6fef0a2ed0be14005e1c5c2c565d62bc
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-02-11 22:16:44 -0500

    Refactor file and directory names, and update placeholders

--- template.py
+++ template.py
@@ -9,17 +9,31 @@ import subprocess
 def convert_placeholders(path: str, project_name: str):
 
     for root, _, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
 
-            with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
+        for f in files:
+            if f.startswith('.'):
+                continue
+
+            file_path = os.path.join(root, f)
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                content = f.read()
 
             content = content.replace('projectslug', project_name)
 
-            with open(file_path, 'w') as file:
-                file.write(content)
+            with open(file_path, 'w') as f:
+                f.write(content)
 
+
+    for root, dirs, _ in os.walk(path):
+        for d in dirs:
+            if 'projectslug' not in d:
+                continue
+
+            os.rename(
+                os.path.join(root, d),
+                os.path.join(root, d.replace('projectslug', project_name))
+            )
 
 def setup_parser():
 
@@ -43,13 +57,22 @@ if __name__ == '__main__':
         subprocess.run(['git', 'init'], cwd=ARGS.path)
 
     out = f'{ARGS.path}/{ARGS.name}'
+    
+    if os.path.exists(out):
+        shutil.rmtree(out)
 
     shutil.copytree(
         os.path.dirname(os.path.abspath(__file__)), out,
         ignore=shutil.ignore_patterns(
-            ARGS.path, '__pycache__', '.git', '.venv', '*.pyc', 'template.py', 
+            ARGS.path, '__pycache__', '.git', '.venv', 'template.py', 
             '*.lock'
-        )
+        ), dirs_exist_ok=True
     )
+
+    for root, dirs, files in os.walk(out):
+        for f in files:
+            if f.endswith('.pyc'):
+                print('➡ file_path:', f)
+
     convert_placeholders(out, ARGS.name)
 
